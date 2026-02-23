5162a52806a89e26b408fb2e949301eca520f1c5
Vítor Garcia Comissoli <90667151+Vitor-Garcia-Comissoli@users.noreply.github.com>
2026-02-23 20:27:48 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -130,8 +130,8 @@ const translations = {
     'hero-badge':         'Disponível para oportunidades',
     'hero-title':         'Bacharel em Estatística & Cientista e Analista de Dados',
     'hero-description':   'Bacharel em Estatística pelo IME-USP. Ex-pesquisador de iniciação científica em Acústica Urbana na FAU USP e Líder da Equipe de Dados na IME Jr. Apaixonado por aplicar estatística a problemas do mundo real e colaborar com equipes inovadoras.',
-    'hero-cta-primary':   'Entre em contato',
-    'hero-cta-secondary': 'Meu trabalho',
+    'hero-cta-primary':   'Contato',
+    'hero-cta-secondary': 'Veja meu trabalho',
 
     // Sobre
     'about-label':    'Quem sou eu',
